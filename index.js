4377f18f791ddf046bab2e7fe6773e1b968082ca
Stephen Thurman <thurman024@gmail.com>
2017-12-02 16:22:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var newArray = []
   for (let i=0; i<=musicians.length; i++) {
-    newArray.push(musicians[i] + "plays" + instruments[i])
+    newArray.push(musicians[i] + " plays " + instruments[i])
   }
   return newArray
 }
